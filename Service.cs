4bff13a6725c768fe8a140c2b30b57602ea12381
mbugowski1 <mbugowski1@wp.pl>
2021-10-21 15:22:22 +0200

    Cryptography exception control

--- Service.cs
+++ Service.cs
@@ -36,11 +36,22 @@ namespace WAT_Planner
             Schedule[] schedules;
             Task<CalendarConnection[]> calendar = Calendar();
 
+            (string, string) credentials;
+            try
+            {
+                credentials = LoadPassword();
+            }
+            catch (System.Security.Cryptography.CryptographicException e)
+            {
+                Debug.WriteLine("ERROR! " + e.Message);
+                ((HostControl)hostControl).Stop();
+                return;
+            }
+            Page page = new Page(credentials.Item1, credentials.Item2);
+            credentials.Item1 = null;
+            credentials.Item2 = null;
 
-            Task<Schedule>[] wat = new Task<Schedule>[groups.Length];
-            for (int i = 0; i < groups.Length; i++)
-                wat[i] = LoadWat();
-            schedules = await Task.WhenAll(wat);
+            schedules = await LoadWat(page, groups);
             calendars = await calendar;
 
 
@@ -57,13 +68,16 @@ namespace WAT_Planner
             }
             ((HostControl)hostControl).Stop();
         }
-        async Task<Schedule> LoadWat()
+
+        async Task<Schedule[]> LoadWat(Page page, string[] groups)
         {
-            (string, string) credentials = LoadPassword();
-            Page page = new Page(credentials.Item1, credentials.Item2);
-            credentials.Item1 = null;
-            credentials.Item2 = null;
-            return await page.LoadSchedule("WCY20IB1S4", 2021, 1);
+            Schedule[] schedules = new Schedule[groups.Length];
+            Task<Schedule>[] scheduleTasks = new Task<Schedule>[groups.Length];
+            for (int i = 0; i < groups.Length; i++)
+                scheduleTasks[i] = page.LoadSchedule(groups[i], 2021, 1);
+            for(int i = 0; i < groups.Length; i++)
+                schedules[i] = await scheduleTasks[i];
+            return schedules;
         }
         async Task<CalendarConnection[]> Calendar()
         {
