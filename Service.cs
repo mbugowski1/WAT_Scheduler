028c3d1174d49039e0745327962061a82f517f47
mbugowski1 <mbugowski1@wp.pl>
2021-11-15 15:55:02 +0100

    Fixed exporting schedule to Google Calendar.

--- Service.cs
+++ Service.cs
@@ -4,11 +4,13 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Topshelf;
+using System.Linq;
 
 namespace WAT_Planner
 {
     class Service : ServiceControl
     {
+        string[] groups = { "WCY19IJ4S1" };
         public bool Start(HostControl hostControl)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
@@ -34,8 +36,7 @@ namespace WAT_Planner
         {
             //string[] groups = { "WCY19IG1S1", "WCY19IJ4S1", "WCY19KC1S1" };
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            string[] groups = { "WCY19IJ4S1" };
-            //Task<CalendarConnection[]> calendarsTask = Calendar();
+            Task<CalendarConnection[]> calendarsTask = Calendar();
             Schedule[] schedules;
 
             (string, string) credentials;
@@ -53,11 +54,8 @@ namespace WAT_Planner
             credentials.Item1 = null;
             credentials.Item2 = null;
             schedules = new Schedule[] { await page.LoadSchedule(groups[0], 2021, 1, Encoding.GetEncoding("ISO-8859-2").GetString(Password.LoadFile("C:/Users/Michal/Desktop/J4.html"))) };
-            //CalendarConnection[] calendars = await calendarsTask;
-            Console.WriteLine(schedules.Length);
+            CalendarConnection[] calendars = await calendarsTask;
             foreach (Schedule schedule in schedules)
-                Console.WriteLine(schedule.ToString(15, 11, 2021));
-            /*foreach (Schedule schedule in schedules)
             {
                 for (int i = 0; i < calendars.Length; i++)
                 {
@@ -67,7 +65,7 @@ namespace WAT_Planner
                         break;
                     }
                 }
-            }*/
+            }
             ((HostControl)hostControl).Stop();
         }
 
@@ -84,7 +82,7 @@ namespace WAT_Planner
         async Task<CalendarConnection[]> Calendar()
         {
             await CalendarConnection.Connect();
-            return await CalendarConnection.GetCalendars(new string[] { "WCY20IB1S4" });
+            return await CalendarConnection.GetCalendars(groups);
         }
     }
 }
