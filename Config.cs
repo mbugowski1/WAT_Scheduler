1d82422fe64476e147f58d24e312f73ee2f471b2
mbugowski1 <mbugowski1@wp.pl>
2021-11-19 01:41:34 +0100

    Changed config settings type. Changed constructor for config.

--- Config.cs
+++ Config.cs
@@ -9,16 +9,33 @@ namespace WAT_Planner
 {
     class Config
     {
+        private class Setting
+        {
+            public bool Brackets { private set; get; }
+            public object Value { private set; get; }
+            public Setting(object value, bool brackets)
+            {
+                Value = value;
+                Brackets = brackets;
+            }
+            public void SetValue(object value, bool brackets)
+            {
+                Value = value;
+                Brackets = brackets;
+            }
+        }
         static readonly string[] defaultSettings = new string[]
-        { 
+        {
             "Login",
             "Groups",
             "SubjectFromGroup",
             "ManualAdd",
             "ManualDelete"
         };
-        Dictionary<string, string[]> settings = new Dictionary<string, string[]>();
-        Dictionary<string, Entry[]> manual = new Dictionary<string, Entry[]>();
+        Dictionary<string, Setting[]> settings = new Dictionary<string, Setting[]>();
+        //Dictionary<string, string[]> settings = new Dictionary<string, string[]>();
+        //Dictionary<string, Dictionary<string, string>> settings = new Dictionary<string, Dictionary<string, string>>();
+        //Dictionary<string, Entry[]> manual = new Dictionary<string, Entry[]>();
         public Config(in string file)
         {
             if (!File.Exists(file))
@@ -27,48 +44,60 @@ namespace WAT_Planner
             }
             else
             {
+                //Remove white symbols and split brackets
                 string config = Encoding.UTF8.GetString(File.ReadAllBytes(file));
-                config = removeNextLineFromBrackets(config, new char[] { ' ', '\t', '\r' });
-                List<string> lines = config.Split('\n').ToList();
-                if (lines[lines.Count - 1].Length != 0) File.AppendAllText(file, Environment.NewLine);
-                lines.ForEach(line =>
-                {
-                    line = line.Split("#", 2)[0];
-                    if (line.Contains('='))
-                    {
-                        string[] seperated = line.Split('=', 2);
-                        if (seperated[1].Contains('{') && seperated[1].Contains('}'))
-                        {
-                            if (seperated[0] != "ManualDelete" && seperated[0] != "ManualAdd") return;
-                            int stop = 0;
-                            List<Entry> values = new List<Entry>();
-                            while(seperated[1].IndexOf('{', stop) != -1)
-                            {
-                                int start = seperated[1].IndexOf('{', stop) + 1;
-                                stop = seperated[1].IndexOf('}', start);
-                                values.Add(GetEntry(seperated[1].Substring(start, stop - start)));
-                            }
-                            if (settings.ContainsKey(seperated[0])) return;
-                            manual.Add(seperated[0], values.ToArray());
-                        }
-                        else
-                        {
-                            string[] values = seperated[1].Split(',');
-                            //for (int i = 0; i < values.Length; i++)
-                            //    values[i] = String.Concat(values[i].Where(c => !Char.IsWhiteSpace(c)));
-                            if (settings.ContainsKey(seperated[0])) return;
-                            settings.Add(seperated[0], values);
-                        }
-                    }
-                });
+                config = PullBrackets(RemoveChars(config, new char[] { ' ', '\t', '\r' }));
+                //Seperate lines
+                string[] lines = config.Split('\n');
+                //Add newline at the end
+                if (lines[lines.Length - 1].Length != 0)
+                    File.AppendAllText(file, Environment.NewLine);
+
+                LoadSettings(lines);
             }
             SettingsCheck(file);
         }
-        string removeNextLineFromBrackets(in string text, char[] excluded)
+        void LoadSettings(string[] lines)
+        {
+            foreach (string edit in lines)
+            {
+                string line = edit.Split("#", 2)[0];
+                if (line.Contains('='))
+                {
+                    string[] seperated = line.Split('=', 2);
+                    if (settings.ContainsKey(seperated[0])) return;
+                    Setting[] sets;
+                    if (seperated[1].Contains('{') && seperated[1].Contains('}'))
+                    {
+                        int stop = 0;
+                        List<Dictionary<string, string>> pairs = new List<Dictionary<string, string>>();
+                        while (seperated[1].IndexOf('{', stop) != -1)
+                        {
+                            int start = seperated[1].IndexOf('{', stop) + 1;
+                            stop = seperated[1].IndexOf('}', start);
+                            string textToEdit = seperated[1].Substring(start, stop - start);
+                            pairs.Add(GetDictionary(textToEdit));
+                        }
+                        sets = new Setting[pairs.Count];
+                        for (int i = 0; i < sets.Length; i++)
+                            sets[i] = new Setting(pairs.ToArray(), true);
+                    }
+                    else
+                    {
+                        string[] values = seperated[1].Split(',');
+                        sets = new Setting[values.Length];
+                        for (int i = 0; i < values.Length; i++)
+                            sets[i] = new Setting(values[i], false);
+                    }
+                    settings.Add(seperated[0], sets);
+                }
+            }
+        }
+        string PullBrackets(in string text)
         {
             int startIndex = 0;
             StringBuilder modifier = new StringBuilder(text);
-            for(int i = 0; i < modifier.Length; i++)
+            for (int i = 0; i < modifier.Length; i++)
             {
                 if (modifier[i] == '{')
                 {
@@ -79,6 +108,11 @@ namespace WAT_Planner
                     modifier.Replace("\n", "", startIndex, i - startIndex);
                 }
             }
+            return modifier.ToString();
+        }
+        string RemoveChars(in string text, char[] excluded)
+        {
+            StringBuilder modifier = new StringBuilder(text);
             for (int i = 0; i < modifier.Length; i++)
             {
                 if(excluded.Contains(modifier[i]))
@@ -97,6 +131,18 @@ namespace WAT_Planner
         {
             string[] timeStrings = time.Split(':', 2);
             DateTime result = new DateTime(date.Year, date.Month, date.Day, Int32.Parse(timeStrings[0]), Int32.Parse(timeStrings[1]), 0);
+            return result;
+        }
+        Dictionary<string, string> GetDictionary(string text)
+        {
+            string[] options = text.Split(',');
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            foreach (string option in options)
+            {
+                if (!option.Contains('=')) continue;
+                string[] values = option.Split('=', 2);
+                result.Add(values[0], values[1]);
+            }
             return result;
         }
         Entry GetEntry(string text)
